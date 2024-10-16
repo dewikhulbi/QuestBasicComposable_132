93981b785f4b95b8051d5c5bd4fda1840b21b270
dewikhulbi <dewikhlb20@gmail.com>
2024-10-16 21:03:52 +0700

    mengatur gaya teks untuk dewi khulbi utami

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -51,3 +51,5 @@ fun BasicLayout(modifier: Modifier = Modifier) {
         Text(text = "Nama", style = TextStyle(fontSize = 40.sp),
             modifier = Modifier.padding(top=20.dp))
         Text(text = "Dewi Khulbi Utami",color = Color.Red,
+            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+        )
