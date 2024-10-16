1aa42154e13b3d37fcb8d749979ca2def6b1accb
dewikhulbi <dewikhlb20@gmail.com>
2024-10-16 20:54:39 +0700

    modifiew mengisi seluruh ukuran layar

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -38,3 +38,6 @@ class MainActivity : ComponentActivity() {
 fun BasicLayout(modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    )
